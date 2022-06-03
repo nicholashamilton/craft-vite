6709084677c7cd31f2c32a4b399d991e9f5361d6
Nick Hamilton <nicholashamilton.co@gmail.com>
2022-06-03 00:56:04 -0400

    Update lit example

--- templates/_generic-components/lit-component/lit-component.ts
+++ templates/_generic-components/lit-component/lit-component.ts
@@ -1,28 +1,38 @@
 import { LitElement, html, css } from 'lit';
+import { property } from 'lit/decorators.js';
 
 export default class LitComponent extends LitElement {
+    @property({
+        type: Number,
+        hasChanged: (value?: number, oldValue?: number) => {
+          return value !== oldValue;
+        }
+    })
     private _count: number = 0;
 
     static styles = css`
-    :host {
-        display: inline-flex;
-        align-items: center;
-        background: rgba(0, 0, 0, .05);
-        padding: 1rem;
-    }
-    
-    :host span {
-        margin: .5rem;
-    }`;
+        :host {
+            display: inline-flex;
+            align-items: center;
+            background: rgba(0, 0, 0, .05);
+            padding: 1rem;
+        }
+        
+        :host span {
+            margin: .5rem;
+        }
+
+        :host button {
+            cursor: pointer;
+        }
+    `;
 
     private increaseCount(): void {
-        this._count = this._count + 1;
-        this.requestUpdate();
+        this._count++;
     }
 
     private decreaseCount(): void {
-        this._count = this._count - 1;
-        this.requestUpdate();
+        this._count--;
     }
 
     render() {
